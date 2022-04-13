c36ee61f190207b34f28e7528e492cf2b9f0ef7a
Duš <62700665+G-rox-y@users.noreply.github.com>
2022-04-13 22:26:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,15 +7,20 @@
 struct node {
     float x, y, velocity, angle;
 };
+struct line {
+    int p1, p2;
+    int age;
+};
+
 int W= 1920, H=1080;
 const float PI = 3.14159f;
-const float WW = 0.1f; // precision
 std::vector<node> nodesData;
 std::vector<std::pair<int, int> > linesData;
 
 //check if the line defined by 2 argument points interceprs with any other lines
 bool intercepts_with_lines(int loc1, int loc2) {
     using namespace std;
+    const float WW = 0.1f; // precision
 
     //shortening the names just so its easier to work with
     float x1 = nodesData[loc1].x;
@@ -23,25 +28,33 @@ bool intercepts_with_lines(int loc1, int loc2) {
     float x2 = nodesData[loc2].x;
     float y2 = nodesData[loc2].y;
 
-    // if (abs(x1-x2) < 2) return true;
-
     for(int i = 0; i < linesData.size(); i++){
-        float x3 = nodesData[linesData[i].first].x;
-        float y3 = nodesData[linesData[i].first].y;
-        float x4 = nodesData[linesData[i].second].x;
-        float y4 = nodesData[linesData[i].second].y;
+        int p3 = linesData[i].first;
+        int p4 = linesData[i].second;
+
+        //interception check exception
+        if (loc1 == p3 || loc1 == p4 || loc2 == p3 || loc2 == p4) continue;
+
+        float x3 = nodesData[p3].x;
+        float y3 = nodesData[p3].y;
+        float x4 = nodesData[p4].x;
+        float y4 = nodesData[p4].y;
 
         //y = ax + b
         //L - Location line, T - Target line
         float aL = (y2-y1) / (x2-x1);
-        float bL = y1 - aL*x1;
+        float bL = (isinf(aL)) ? x1 : y1 - aL*x1;
         float aT = (y4-y3) / (x4-x3);
-        float bT = y3 - aT*x3;
+        float bT = (isinf(aT)) ? x3 : y3 - aT*x3;
 
         float x = (bT-bL) / (aL-aT);
+        float yT = (isinf(aT)) ? bT : aT*x+bT;
+        float yL = (isinf(aL)) ? bL : aL*x+bT;
 
-        if (x-min(x1,x2)-WW > 0 && x-max(x1,x2)+WW < 0 && x-min(x3,x4)-WW > 0 && x-max(x3,x4)+WW < 0)
-            return true;
+        //interception checks
+        if (abs(x1-x2)<WW && min(x3,x4)<min(x1,x2) && max(x3,x4)>max(x1,x2) && yT>min(y1,y2) && yT<max(y1,y2)) return true;
+        if (abs(x3-x4)<WW && min(x1,x2)<min(x3,x4) && max(x1,x2)>max(x3,x4) && yL>min(y3,y4) && yL<max(y3,y4)) return true;
+        if (x-min(x1,x2)>WW && x-min(x3,x4)>WW && max(x1,x2)-x>WW && max(x3,x4)-x>WW) return true;
     }
 
     return false;
@@ -77,32 +90,25 @@ void update_nodes_data() {
     linesData.clear();
 
     //connects nodes
-    struct line {
+    struct lineSpecial {
         int p1, p2;
         float length;
     };
 
-    vector<line> v;
+    vector<lineSpecial> v;
     for (int i = 0; i < nodesData.size(); i++){
         for (int j = i+1; j < nodesData.size(); j++){
-            line l;
+            lineSpecial l;
             l.p1 = i; l.p2 = j;
             l.length = sqrt(pow(nodesData[i].x-nodesData[j].x, 2) + pow(nodesData[i].y-nodesData[j].y, 2));
             v.emplace_back(l);
         }
     }
-    sort(v.begin(), v.end(), [](line& b1, line& b2) -> bool {return b1.length < b2.length;} );
+    sort(v.begin(), v.end(), [](lineSpecial& b1, lineSpecial& b2) -> bool {return b1.length < b2.length;} );
 
     for(int i = 0; i < v.size(); i++)
         if (!intercepts_with_lines(v[i].p1, v[i].p2))
             linesData.emplace_back(make_pair(v[i].p1, v[i].p2));
-    
-    vector<pair<int, int> > linesData2;
-    for(int i = 0; i < linesData.size(); i++)
-        if (abs(nodesData[linesData[i].first].x - nodesData[linesData[i].second].x) > 1)
-            linesData2.emplace_back(linesData[i]);
-            
-    linesData = linesData2;
 
     return;
 }
@@ -139,7 +145,7 @@ void window_setup() {
     VideoMode desktop = VideoMode::getDesktopMode();
     RenderWindow window(VideoMode(W, H, desktop.bitsPerPixel), "Polysim");
     View view(Vector2f(W/2, H/2), Vector2f(W, H));
-    window.setFramerateLimit(24);
+    window.setFramerateLimit(12);
     while(window.isOpen()){
 
         Event event;
@@ -149,11 +155,11 @@ void window_setup() {
 
         window.clear();
         window.setView(view);
-        CircleShape dot(2.0f);
-        for(int i = 0; i < nodesData.size(); i++){
-            dot.setPosition(nodesData[i].x-1, nodesData[i].y-1);
-            window.draw(dot);
-        }
+        // CircleShape dot(2.0f);
+        // for(int i = 0; i < nodesData.size(); i++){
+        //     dot.setPosition(nodesData[i].x-1, nodesData[i].y-1);
+        //     window.draw(dot);
+        // }
 
         for(int i = 0; i < linesData.size(); i++){
             float x1 = nodesData[linesData[i].first].x;
