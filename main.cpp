f2d81d838a758c01413b189c0c938c6764f5aa70
Duš <62700665+G-rox-y@users.noreply.github.com>
2022-04-15 19:54:38 +0200

    Minor changes
    Added stationary points

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 
 struct node {
     float x, y, velocity, angle;
+    int type;
+    // type == 1 point constrained inside the window
+    // type == 0 outside window and static
 };
 struct line {
     int p1, p2;
@@ -50,7 +53,7 @@ bool intercepts_with_lines(int loc1, int loc2) {
         
         if (abs(x1-x2) < WW){
             float y = aT*x1+bT;
-            if (y > min(y3, y4) && y < max(y3, y4)) return true;
+            if (y > min(y3,y4) && y < max(y3,y4)) return true;
         }
         if (abs(x3-x4) < WW){
             float y = aL*x3+bL;
@@ -70,6 +73,7 @@ void update_nodes_data() {
     using namespace std;
     //position update
     for(int i = 0; i < nodesData.size(); i++){
+        if (!nodesData[i].type) continue;
         float yChange = sin(nodesData[i].angle*2*PI/360) * nodesData[i].velocity;
         float xChange = cos(nodesData[i].angle*2*PI/360) * nodesData[i].velocity;
 
@@ -104,6 +108,7 @@ void update_nodes_data() {
     vector<lineSpecial> v;
     for (int i = 0; i < nodesData.size(); i++){
         for (int j = i+1; j < nodesData.size(); j++){
+            if (!nodesData[i].type && !nodesData[j].type) continue;
             lineSpecial l;
             l.p1 = i; l.p2 = j;
             l.length = sqrt(pow(nodesData[i].x-nodesData[j].x, 2) + pow(nodesData[i].y-nodesData[j].y, 2));
@@ -120,23 +125,48 @@ void update_nodes_data() {
 }
 
 // generates random nodes
-void node_spawner(int nodeAmmount){
+void node_spawner(int insideNodeAmmount){
     using namespace std;
 
     random_device dev;
     mt19937 rng(dev());
     uniform_real_distribution<> randomX(0.0f, (float)W);
     uniform_real_distribution<> randomY(0.0f, (float)H);
-    uniform_real_distribution<> randomVel(0.2f, 0.8f);
+    uniform_real_distribution<> randomVel(0.2f, 0.7f);
     uniform_real_distribution<> randomAng(0.0f, 360.0f);
 
-    for(int i=0; i<nodeAmmount; i++){
+    for(int i=0; i< insideNodeAmmount; i++){
         node n;
         n.x = randomX(rng);
         n.y = randomY(rng);
         n.velocity = randomVel(rng);
         n.angle = randomAng(rng);
+        n.type = 1;
         nodesData.emplace_back(n);
+    }
+
+    for(int i = 0; i <= 10; i++){
+        node n1, n2, n3, n4;
+
+        n1.x = W*i/10;
+        n1.y = -10;
+        n1.type = 0;
+        nodesData.emplace_back(n1);
+        
+        n2.x = W*i/10;
+        n2.y = H+10;
+        n2.type = 0;
+        nodesData.emplace_back(n2);
+        
+        n3.y = H*i/10;
+        n3.x = -10;
+        n3.type = 0;
+        nodesData.emplace_back(n3);
+
+        n4.y = H*i/10;
+        n4.x = W+10;
+        n4.type = 0;
+        nodesData.emplace_back(n4);
     }
 
     //called so that nodes are connected from the start
@@ -150,7 +180,7 @@ void window_setup() {
 
     VideoMode desktop = VideoMode::getDesktopMode();
     RenderWindow window(VideoMode(W, H, desktop.bitsPerPixel), "Polysim");
-    View view(Vector2f(W/2, H/2), Vector2f(W, H));
+    View view(Vector2f(W/2, H/2), Vector2f(W*2/3, H*2/3));
     window.setFramerateLimit(24);
     while(window.isOpen()){
 
@@ -182,7 +212,7 @@ void window_setup() {
 }
 
 int main() {
-    node_spawner(100);
+    node_spawner(150);
     window_setup();
     return 0;
 }
